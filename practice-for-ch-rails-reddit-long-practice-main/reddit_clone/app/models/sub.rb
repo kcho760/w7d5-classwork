c9b0a7290e3eb4187d5c28264d370ba039a2029b
lewiskk016 <lewiskk016@gmail.com>
2023-04-21 15:50:13 -0400

    forms

--- practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/models/sub.rb
+++ practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/models/sub.rb
@@ -1,7 +1,7 @@
 class Sub < ApplicationRecord
-    # has_many :posts,
-    #     foreign_key: :post_id,
-    #     class_name: :Post
+    has_many :posts,
+        foreign_key: :post_id,
+        class_name: :Post
 
     has_one :moderator,
         foreign_key: :user_id,
