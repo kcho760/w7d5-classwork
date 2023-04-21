77c3332f6a3017ff2ef17c6eea10689c2dcaaa4a
kcho760 <kcho760@gmail.com>
2023-04-21 14:00:08 -0400

    First commit

---
+++ practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+#SPIRE
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def generate_unique_session_token
+        session_token = RandomSecure::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+end
